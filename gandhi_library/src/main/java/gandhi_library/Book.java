256b3fb4a7313a7f15db47fa018d1222feb88ecf
amit <amit17031999@gmail.com>
2021-10-07 22:39:58 +0530

    deleted somecode

--- gandhi_library/src/main/java/gandhi_library/Book.java
+++ gandhi_library/src/main/java/gandhi_library/Book.java
@@ -1,20 +1,7 @@
 package gandhi_library;
 
 public class Book {
-     private String bookName;
-     private int id;
-	public Book(String bookName, int id) {
-		super();
-		this.bookName = bookName;
-		this.id = id;
-	}
-	public Book() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
-	public String getBookName() {
-		return bookName;
-	}
+
 	public void setBookName(String bookName) {
 		this.bookName = bookName;
 	}
